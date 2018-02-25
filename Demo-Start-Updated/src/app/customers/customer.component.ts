8a6d747a5e91bb5b7d0c4eafa0ded80a0e27823e
richie.buckingham <richie.buckingham@gmail.com>
2018-02-25 15:15:04 +1100

    building a reactive form

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -7,11 +7,32 @@ import { Customer } from './customer';
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent implements OnInit  {
+    public customerForm: FormGroup;
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    constructor(private formBuilder: FormBuilder){ }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm));
+    }
+
+    public ngOnInit(): void {
+        this.customerForm = this.formBuilder.group({
+            firstName: '',
+            lastName: '',
+            email: '',
+            sendCatalog: true,
+        });
+    }
+
+    public populateTestData(): void {
+        this.customerForm.patchValue({
+            firstName: "rich",
+            lastName: "buck",
+            email: "test@test.com"//,
+           // sendCatalog: false
+        })
     }
  }
